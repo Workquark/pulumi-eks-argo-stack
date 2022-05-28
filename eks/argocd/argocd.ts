623fc93b3c9f516fca0f01b41186bbe916cccfdc
Joydeep Roychowdhury <joydeep.roychowdhury@workquark.com>
2022-05-29 02:14:25 +0530

    Removed unnecessary data

--- eks/argocd/argocd.ts
+++ eks/argocd/argocd.ts
@@ -32,35 +32,11 @@ export class ArgoCD extends pulumi.ComponentResource {
         const guestbook = new k8s.yaml.ConfigFile("argocd", {
             file: "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml",
             transformations: [
-
                 (obj: any, opts: pulumi.CustomResourceOptions) => {
                     if (obj.metadata) {
                         obj.metadata.namespace = "argocd"
                     }
-                },
-                // // Make every service private to the cluster, i.e., turn all services into ClusterIP instead of LoadBalancer.
-                // (obj: any, opts: pulumi.CustomResourceOptions) => {
-                //     if (obj.kind === "Service" && obj.apiVersion === "v1") {
-                //         if (obj.spec && obj.spec.type && obj.spec.type === "LoadBalancer") {
-                //             obj.spec.type = "ClusterIP";
-                //         }
-                //     }
-                // },
-
-                // // Set a resource alias for a previous name.
-                // (obj: any, opts: pulumi.CustomResourceOptions) => {
-                //     if (obj.kind === "Deployment") {
-                //         opts.aliases = [{ name: "oldName" }]
-                //     }
-                // },
-
-                // // Omit a resource from the Chart by transforming the specified resource definition to an empty List.
-                // (obj: any, opts: pulumi.CustomResourceOptions) => {
-                //     if (obj.kind === "Pod" && obj.metadata.name === "test") {
-                //         obj.apiVersion = "v1"
-                //         obj.kind = "List"
-                //     }
-                // },
+                }
             ],
         }, {
             dependsOn: [argoCDNamespace]
